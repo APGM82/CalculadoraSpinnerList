c4de5ace379533033183b7ae969088361c096441
APGM82 <90765662+APGM82@users.noreply.github.com>
2022-10-21 11:47:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculadoraspinnerlist/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.calculadoraspinnerlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.calculadoraspinnerlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        var vec=ArrayList<String>(4)
+        vec.add("Suma")
+        vec.add("Resta")
+        vec.add("Multiplicación")
+        vec.add("División")
+        val adaptador= ArrayAdapter(this,R.layout.item_lista,R.id.txtItem,vec)
+        binding.spLista.adapter=adaptador
+
+        binding.spSpinner.adapter=adaptador
+
+
+        binding.spLista.setOnItemClickListener { adapterView, view, i, l ->
+            when(i){
+                0 ->
+                    try {
+                        binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString()).toFloat()+(Integer.parseInt(binding.txtOperador2.text.toString()).toFloat())).toString())
+
+                    }catch (e:java.lang.Exception){
+                        Toast.makeText(this, "El número introducido no es válido", Toast.LENGTH_SHORT).show()
+                    }
+
+                1 ->
+                    try {
+                        binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString()).toFloat()-(Integer.parseInt(binding.txtOperador2.text.toString()).toFloat())).toString())
+                    }catch (e:java.lang.Exception){
+                        Toast.makeText(this, "El número introducido no es válido", Toast.LENGTH_SHORT).show()
+                    }
+                2 ->
+                    try {
+                        binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString()).toFloat()*(Integer.parseInt(binding.txtOperador2.text.toString()).toFloat())).toString())
+                    }catch (e:java.lang.Exception){
+                        Toast.makeText(this, "El número introducido no es válido", Toast.LENGTH_SHORT).show()
+                    }
+
+                3 ->
+                    try {
+                        binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString()).toFloat()/(Integer.parseInt(binding.txtOperador2.text.toString()).toFloat())).toString())
+                    }catch (e:java.lang.Exception){
+                        Toast.makeText(this, "El número introducido no es válido", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+        //He buscado la extension necesario para que cuando selecciones un elemento se active la función pero no he podido encontrarla
+        /*binding.spSpinner.setOnItemClickListener { adapterView, view, i, l ->
+            when(i){
+                0 -> binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString())+(Integer.parseInt(binding.txtOperador2.text.toString()))).toString())
+                1 -> binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString())-(Integer.parseInt(binding.txtOperador2.text.toString()))).toString())
+                2 -> binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString())*(Integer.parseInt(binding.txtOperador2.text.toString()))).toString())
+                3 -> binding.textView2.text=((Integer.parseInt(binding.txtOperador1.text.toString())+(Integer.parseInt(binding.txtOperador2.text.toString()))).toString())
+
+            }
+        }*/
+
+    }
+}
